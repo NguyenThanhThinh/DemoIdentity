362afe641e4b0ba410c33cefe1fbac51342e2d1e
Thinh Nguyen <thanhthinhcntt@gmail.com>
2018-06-21 22:55:34 +0700

    add method course

--- DemoIdentity/DemoIdentity.Services/Interfaces/ICourseService.cs
+++ DemoIdentity/DemoIdentity.Services/Interfaces/ICourseService.cs
@@ -1,10 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace DemoIdentity.Services.Interfaces
 {
+    using DemoIdentity.Services.Models;
     public interface ICourseService
     {
+        Task<IEnumerable<CourseListingServiceModel>> GetAllAsync();
+
+        Task<TModel> GetByIdAsync<TModel>(int id) where TModel : class;
+
+        Task<bool> IsUserSignedInCourseAsync(int courseId, string userId);
+
+        Task<bool> SignUpStudentAsync(int courseId, string userId);
+
+        Task<bool> SignOutStudentAsync(int courseId, string userId);
     }
 }
